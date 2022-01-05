dcf0473add27f1b207a9932693d860c8feb61357
Anes <marzoukianas@gmail.com>
2022-01-05 16:12:18 +0100

    Linear interpolation approach

---
+++ src/Cursor.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import './cursor.css';
+
+const Cursor = () => {
+  const delay = 9;
+
+  const cursorVisible = React.useRef(true);
+  const cursorEnlarged = React.useRef(false);
+
+  // Position of mouse.
+  const mouseX = React.useRef(window.innerWidth / 2);
+  const mouseY = React.useRef(window.innerHeight / 2);
+
+  // Position of cursor outline.
+  const cursorX = React.useRef(0);
+  const cursorY = React.useRef(0);
+
+  const requestRef = React.useRef(null);
+  const outline = React.useRef(null);
+
+  const toggleCursorVisibility = () => {
+    if (cursorVisible.current) {
+      outline.current.style.opacity = 1;
+    } else {
+      outline.current.style.opacity = 0;
+    }
+  };
+
+  const mouseMoveEvt = (e) => {
+    cursorVisible.current = true;
+    toggleCursorVisibility();
+
+    mouseX.current = e.pageX;
+    mouseY.current = e.pageY;
+  };
+
+  const animateOutline = () => {
+    console.log('animate')
+    cursorX.current += (mouseX.current - cursorX.current) / delay;
+    cursorY.current += (mouseY.current - cursorY.current) / delay;
+
+    outline.current.style.top = `${cursorY.current}px`;
+    outline.current.style.left = `${cursorX.current}px`;
+
+      requestRef.current = requestAnimationFrame(animateOutline);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('mousemove', mouseMoveEvt);
+    animateOutline();
+    return () => {
+      document.removeEventListener('mousemove', mouseMoveEvt);
+      cancelAnimationFrame(requestRef.current);
+    }
+  }, [])
+  return (
+    <>
+      <div className="cursor-outline" ref={outline}></div>
+    </>
+  )
+};
+
+export default Cursor;
---
+++ src/Kursor.js
@@ -0,0 +1,112 @@
+import React, {useEffect, useRef} from 'react';
+import './kursor.css';
+
+const initConfig = {
+  x: {
+    previous: 0,
+    current: 0,
+    amt: 0.1
+  },
+  y: {
+    previous: 0,
+    current: 0,
+    amt: 0.1
+  }
+};
+
+const lerp = (x, y, n) => (1 - n) * x + n * y;
+
+// Gets the mouse position
+const getMousePos = (mouseEvent) => {
+    let posx = 0;
+    let posy = 0;
+    if (!mouseEvent) mouseEvent = window.event;
+    if (mouseEvent.pageX || mouseEvent.pageY) {
+        posx = mouseEvent.pageX;
+        posy = mouseEvent.pageY;
+    }
+    else if (mouseEvent.clientX || mouseEvent.clientY)    {
+        posx = mouseEvent.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        posy = mouseEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return { x : posx, y : posy }
+};
+
+export default function Kursor() {
+
+  const mouse = useRef({x: 0, y: 0});
+  const configs = useRef(initConfig);
+
+  const requestRef = useRef(null);
+
+  const cursorRef = useRef();
+  const bounds = useRef(null);
+
+  // Get mouse position;
+    const windowMouseMoveHandler = (e) => {
+      mouse.current = getMousePos(e);
+    };
+
+
+  const animateCursor = React.useCallback(() => {
+
+    configs.current['x'].current = mouse.current.x - bounds.current.width / 2;
+    configs.current['y'].current = mouse.current.y - bounds.current.height / 2;
+
+    for (const key in configs.current) {
+      configs.current[key].previous = lerp(configs.current[key].previous, configs.current[key].current, configs.current[key].amt);
+    }
+    // Assigning the cursor's x and y to the HTML cursor element.
+    cursorRef.current.style.transform = `translateX(${(configs.current['x'].previous)}px) translateY(${configs.current['y'].previous}px)`;
+    // cursorRef.current.style.top = `${configs.current.y.previous}px`;
+    // cursorRef.current.style.left = `${configs.current.x.previous}px`;
+
+    requestRef.current = requestAnimationFrame(() => animateCursor);
+    // requestAnimationFrame(() => animateCursor());
+  }, []);
+
+  const onMouseMoveEv = React.useCallback(() => {
+    // console.log('mouse', mouse.current)
+    // console.log('cursor', configs.current)
+
+    // mouse.current = mousePos;
+
+    configs.current.x.previous = configs.current.x.current = mouse.current.x - bounds.current.width / 2;
+    configs.current.y.previous = configs.current.y.previous = mouse.current.y - bounds.current.height / 2;
+    requestRef.current = requestAnimationFrame(() => animateCursor());
+    // window.removeEventListener('mousemove', onMouseMoveEv);
+  }, [animateCursor]);
+
+  useEffect(() => {
+    bounds.current = cursorRef.current.getBoundingClientRect();
+
+    // window.addEventListener('mousemove', mouseMoveEvent);
+    window.addEventListener('mousemove', onMouseMoveEv);
+
+    // animateCursor();
+
+    return () => {
+      // window.removeEventListener('mousemove', mouseMoveEvent);
+      window.removeEventListener('mousemove', onMouseMoveEv);
+      cancelAnimationFrame(requestRef.current);
+    }
+  }, [onMouseMoveEv]);
+
+  useEffect(() => {
+    console.log(configs.current)
+    window.addEventListener('mousemove', windowMouseMoveHandler);
+    return () => {
+      window.removeEventListener('mousemove', windowMouseMoveHandler);
+    }
+  })
+
+  return (
+    <div>
+      <h1>Kursor</h1>
+      <svg ref={cursorRef} className="kursor" width="80" height="80" viewBox="0 0 80 80">
+        <circle className="cursor__inner" cx="40" cy="40" r="20" />
+      </svg>
+    </div>
+  )
+};
